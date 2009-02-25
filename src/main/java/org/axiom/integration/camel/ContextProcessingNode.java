b80f4f937b00db4f178deadad8a83644718bd51a
pax <watson.timothy@gmail.com>
2009-02-25 15:45:21 +0000

    updates to javadocs

--- src/main/java/org/axiom/integration/camel/ContextProcessingNode.java
+++ src/main/java/org/axiom/integration/camel/ContextProcessingNode.java
@@ -3,7 +3,26 @@ package org.axiom.integration.camel;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Processor;
 
+/**
+ * Describes a {@link org.apache.camel.Processor} that contains
+ * a {@link org.apache.camel.CamelContext} which is the target
+ * of incoming processing instructions.
+ *
+ * 
+ */
 public interface ContextProcessingNode extends Processor {
+    
+    /**
+     * Sets the {@link org.apache.camel.CamelContext} to
+     * which processing instructions will be applied.
+     * @param context
+     */
     void setContext(CamelContext context);
+
+    /**
+     * Gets the {@link org.apache.camel.CamelContext} to
+     * which processing instructions are being applied.
+     * @return
+     */
     CamelContext getContext();
 }
